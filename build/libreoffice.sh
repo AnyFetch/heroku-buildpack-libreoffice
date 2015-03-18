ff18372cac0e78b87517b14e975e422ea5f64f4d
Amoki <hugo.duroux@gmail.com>
2015-03-18 18:40:01 +0100

    fix temp_dir

--- build/libreoffice.sh
+++ build/libreoffice.sh
@@ -18,7 +18,7 @@ DEPS_FILE="libreoffice_deps.tar.gz"
 PREFIX=/app/vendor/libreoffice/deps
 mkdir -p $PREFIX
 
-temp_dir=$(mktemp -d tmp/libreoffice.XXXXXXXXXX)
+temp_dir=$(mktemp -d /tmp/libreoffice.XXXXXXXXXX)
 cd ${temp_dir}
 
 # Release dir
@@ -26,8 +26,8 @@ mkdir -p release
 
 # Download and extract the .debs
 curl -L ${DEB_DOWNLOAD_URL} -o libreoffice.tar.gz
-archive_name=$(tar tzf libreoffice.tar.gz | sed -e 's@/.*@@' | uniq)
-tar xzf libreoffice.tar.gz
+archive_name=$(tar tvzf libreoffice.tar.gz | sed -e 's@/.*@@' | uniq)
+tar xvzf libreoffice.tar.gz
 
 cd ${archive_name}
 for f in DEBS/*.deb
@@ -35,7 +35,7 @@ do
   ar p "$f" data.tar.gz | tar zx
 done
 
-tar pczf ${LIBREOFFICE_BINARIES_FILE} opt
+tar pvczf ${LIBREOFFICE_BINARIES_FILE} opt
 mv ${LIBREOFFICE_BINARIES_FILE} "${temp_dir}/release/${LIBREOFFICE_BINARIES_FILE}"
 cd ${temp_dir}
 
@@ -46,7 +46,7 @@ echo "==================================== LibreOffice built, now building depen
 # Download and build gettext
 curl ${GETTEXT_DOWNLOAD_URL} -o gettext.tar.gz
 archive_name=$(tar tzf gettext.tar.gz | sed -e 's@/.*@@' | uniq)
-tar xzf gettext.tar.gz
+tar xvzf gettext.tar.gz
 cd ${archive_name}
 ./configure --prefix=${PREFIX}
 make
@@ -61,7 +61,7 @@ export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
 # Download and build DBus
 curl ${DBUS_DOWNLOAD_URL} -o dbus.tar.gz
 archive_name=$(tar tzf dbus.tar.gz | sed -e 's@/.*@@' | uniq)
-tar xzf dbus.tar.gz
+tar xvzf dbus.tar.gz
 cd ${archive_name}
 ./configure --prefix=${PREFIX}
 make
@@ -71,7 +71,7 @@ cd ${temp_dir}
 # Download and build libffi
 curl ${LIBFFI_DOWNLOAD_URL} -o libffi.tar.gz
 archive_name=$(tar tzf libffi.tar.gz | sed -e 's@/.*@@' | uniq)
-tar xzf libffi.tar.gz
+tar xvzf libffi.tar.gz
 cd ${archive_name}
 ./configure --prefix=${PREFIX}
 make
@@ -81,7 +81,7 @@ cd ${temp_dir}
 # Download and build GLib
 curl ${GLIB_DOWNLOAD_URL} -o glib.tar.xz
 archive_name=$(tar tJf glib.tar.xz | sed -e 's@/.*@@' | uniq)
-tar xJf glib.tar.xz
+tar xvJf glib.tar.xz
 cd ${archive_name}
 ./configure --prefix=${PREFIX}
 make
@@ -91,7 +91,7 @@ cd ${temp_dir}
 # Download and build DBus-GLib
 curl ${DBUS_GLIB_DOWNLOAD_URL} -o dbus-glib.tar.gz
 archive_name=$(tar tzf dbus-glib.tar.gz | sed -e 's@/.*@@' | uniq)
-tar xzf dbus-glib.tar.gz
+tar xvzf dbus-glib.tar.gz
 cd ${archive_name}
 ./configure --prefix=${PREFIX}
 make
@@ -99,7 +99,7 @@ make install
 cd ${temp_dir}
 
 # Compress all dependencies
-tar pczf ${DEPS_FILE} ${PREFIX}
+tar pvczf ${DEPS_FILE} ${PREFIX}
 mv ${DEPS_FILE} release/${DEPS_FILE}
 cd
 rm -r temp_dir
